c407096a17b94b7bf810539abb9f1b02c433b886
Dario Jose Arteaga <dariojose.arteagavillalb@egmail.com>
2023-05-30 16:06:27 -0500

    se editan los componentes de requirements & se agregan CORS

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 import os
+
 
 # Configuración de la aplicación Flask
 app = Flask(__name__)
+CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Dc9CeitEUe6TaleunmNP@containers-us-west-47.railway.app:5525/railway'  # Reemplaza con tus datos de conexión a la base de datos
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
